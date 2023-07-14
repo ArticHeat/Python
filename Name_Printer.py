814d4677789c3ca649bbe795eb4115316ff1857b
Aneesh <articheat1@gmail.com>
2023-07-14 22:07:11 +1000

    Second File

---
+++ Name_Printer.py
@@ -0,0 +1,15 @@
+# File Name: Name_Printer
+# Author Name: Aneesh Vasudevan
+# Email Address: Aneesh1Vasu@gmail.com
+#Enivironment: Python 3
+# Log of Change:
+
+username = input("Enter your name: ")
+
+print(f"Your name is {username}")
+
+fav_num = input("What is your favourite number: ")
+
+for i in range(0,int(fav_num)+1):
+    print(i**2)
+
